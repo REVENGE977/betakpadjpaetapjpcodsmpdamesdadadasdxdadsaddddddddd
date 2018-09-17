209126fe358a1584625e00f8c92261120829146b
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-17 19:24:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,5 +95,64 @@ Sorry. You Got Rejected.**`)
   }
 });
 
+
+console.log(`CodyCodes edit`)
+//code by Codes
+//edit by OrochiX#5426 With ID <429972030092476437>
+
+client.on('message', async message => {  
+    if(message.content.startsWith(prefix + "sharejs")) {      
+      await message.channel.send("**:writing_hand: ارسل الكود الان **").then(e => {
+      let filter = m => m.author.id === message.author.id
+      let lan = '';
+      let md = '';  
+      let br = '';  
+      let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+      .then(collected => {
+        lan = collected.first().content
+        collected.first().delete()
+  e.edit(`**:writing_hand: اكتب وصف الكود الان**`)
+  let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+  .then(co => {  
+    md = co.first().content
+          co.first().delete()  
+          e.edit(`**:writing_hand: ارسل المصدر او صانع الكود**`)
+  let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })    
+  .then(col => {
+    br = col.first().content
+          col.first().delete()
+  e.edit("**جاري النشر ...**").then(b => {    
+          setTimeout(() => {
+    b.edit(`**تم النشر بنجاح**`)
+          },2000);
+  let gg = message.guild.channels.find('name', 'codes-js')
+  if(!gg) return message.reply('لا يوجد روم باسم codes-js')
+  if(gg) {
+  gg.send(`
+  @everyone | @here 
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  Beta Codes© :arrow_down:
+  ${lan}
+
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  
+  **وصف الكود:** ${md}
+  **تم النشر بواسطة:** ${message.author.tag} With ID ${message.author.id}
+  **المصدر / الشخص يلي صنع الكود:** ${br}
+  
+  
+  
+  
+  
+  `)
+  }
+}
+)}
+  )}
+)}
+      )}
+    )}
+})
+
   
   client.login(process.env.BOT_TOKEN)
