67ff1430585aa1f9dc81e81c7705b4f72de8dd69
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-17 19:19:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ console.log('hello')
 [${expe}]
 [${time}]
 [${lang}]`)
-              let rokzz = rokz.guild.channels.find("name", "تقديم")
+              let rokzz = rokz.guild.channels.find("name", "التقديمات")
               setTimeout(() => {
                 e.edit("تم التقديم")
               }, 3000)
@@ -60,5 +60,39 @@ console.log('hello')
   })
   
   
+
+  client.on('message',async message => {
+  let mention = message.mentions.members.first();
+let mySupport = message.guild.roles.find('name', 'Supporter');
+
+  let acRoom = client.channels.get('491271901105094656');
+  if(message.content.startsWith(prefix + "قبول")) {
+    if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+    if(!mention) return message.reply('منشن شخص');
+    if(!mySupport) return message.reply('هذه الرتبة غير موجودة');
+    if(mention.roles.has(mySupport)) return message.reply('هذا الشخص معه الرتبة مسبقا');
+
+    mention.addRole(mySupport).then(() => {
+      acRoom.send(`**${mention} 
+Thanks For Your Apply.
+You Get The Support**`);
+    });
+  }
+});
+  
+
+
+client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  let acRroom = client.channels.get('491271901105094656');
+  if(message.content.startsWith(prefix + "رفض")) {
+  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+  if(!mention) return message.reply("منشن شخص");
+
+  acRroom.send(`**${mention} 
+Sorry. You Got Rejected.**`)
+  }
+});
+
   
   client.login(process.env.BOT_TOKEN)
