2dcfaf5d868d865b35685f09f74f78d5f4161edb
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-17 19:13:35 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+console.log('hello')
+
+
+  client.on('message', async rokz => {
+    if(rokz.content.startsWith(prefix + "تقديم")) {
+      let lang = '';
+      let time = '';
+      let expe = '';
+      let fillter = m => m.author.id === rokz.author.id
+      await rokz.channel.send("ما لغتك ؟").then(e => {
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+     .then(co => {
+       lang = co.first().content;
+        co.first().delete();
+
+       e.edit(`مدة في اللغه
+[${lang}]`)
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+       .then(col => {
+         time = col.first().content;
+          col.first().delete();
+
+            e.edit(`خبرتك ؟
+[${time}]
+[${lang}]`)
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+            .then(coll => {
+              expe = coll.first().content;
+               coll.first().delete();
+
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+              let rokzz = rokz.guild.channels.find("name", "تقديم")
+              setTimeout(() => {
+                e.edit("تم التقديم")
+              }, 3000)
+              rokzz.send(`
+                تقديم من ${rokz.author}
+
+  اللغه:
+  \`${lang}\`
+  المده:
+  \`${time}\`
+  الخبره:
+  \`${expe}\`
+                `).then(rokzzz => {
+                  rokzzz.react("✅")
+                  rokzzz.react("❌")
+                })
+            })
+       })
+     })
+   })
+    }
+  })
+  
+  
+  
+  client.login('NDg4MzczNTI1Mjc5ODAxMzQ0.DoFkIw.7i8VSAnV4Lz2wwRfPyBGO9IXVw8')
